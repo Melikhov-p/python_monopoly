bdc8c612233c788d259a63a83e94c74e45caa329
Melikhov-P <melikhov.pavel@gmail.com>
2022-09-01 17:47:26 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,61 @@
+from mon_classes import *
+
+COMMANDS_BASE = {
+    '1': 'Бросить кубики',
+    'info': 'Информация',
+    '0': 'Закончить игру'
+}
+COMMANDS_FIELD = {
+    '2': 'Купить поле',
+    '0': 'Пропустить'
+}
+
+def main():
+    game = Game()
+    p1 = game.p1
+    p2 = game.p2
+    players = [p1, p2]
+    while True:
+        for player in players:
+            r = need_command(player, game)
+            if r == 'again':
+                need_command(player, game)
+            print('-------------------------------')
+
+
+def need_command(player: Player, game):
+    for com, value in COMMANDS_BASE.items():
+        print(f'{com}: {value}')
+    command = input(f'Команда игрок - ({player.name}): ')
+    if command == '1': # росок кубиков
+        player.roll_the_dice([game.Cub1, game.Cub2])
+        print(f'Позиция: {player.position}')
+        for group in game.game_board.groups:
+            for field in group.fields:
+                if player.position == field.id and field.holder is None:
+                    print(f'ПОЛЕ {field.name} ({field.group.name}) // {field.cost} RUB')
+                    for com, value in COMMANDS_FIELD.items():
+                        print(f'{com}: {value}')
+                    command = input(f'Команда ({player.name}): ')
+                    if command == '2':
+                        player.buy_field(field)
+                        return 'next'
+                    elif command == '0':
+                        break
+                elif player.position == field.id and player is not field.holder:
+                    player.pay_rent(field)
+                    print(f'{player.money} (-{field.rent})')
+                    break
+        return 'next'
+    elif command == 'info':
+        print(f'''
+        ИМЯ: {player.name}
+        СЧЕТ: {player.money}
+        МОИ ПОЛЯ: {[field.name for field in player.fields]}                
+        ''')
+        return 'again'
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ mon_classes.py
@@ -0,0 +1,103 @@
+import random
+
+
+class Board:
+    def __init__(self):
+        self.size = 40
+        self.groups = []
+
+
+class Group:
+    def __init__(self, name, board: Board):
+        self.name = name
+        self.fields = []
+        board.groups.append(self)
+
+
+class Field:
+    def __init__(self, id: int, name: str, cost: int, group: Group, type, rent: int, holder=None):
+        self.id = id
+        self.name = name
+        self.cost = cost
+        self.group = group
+        self.type = type
+        self.rent = rent
+        self.holder = holder
+        self.pledge = self.cost / 2
+        self.pledge_status = False
+        group.fields.append(self)
+
+
+class Player:
+    def __init__(self, id: int, name, money):
+        self.id = id
+        self.money = money
+        self.name = name
+        self.fields = []
+        self.position = 0
+
+    def buy_field(self, field: Field):
+        if self.money >= field.cost and field.holder is None:
+            self.fields.append(field)
+            self.money -= field.cost
+            field.holder = self
+        else:
+            return None
+
+    def lay_field(self, field: Field):
+        if field.holder == self:
+            self.money += field.pledge
+            field.pledge_status = True
+
+    def roll_the_dice(self, Cubes: list):
+        dropped_sides = []
+        for Cube in Cubes:
+            dropped_sides.append(Cube.roll())
+        print(dropped_sides)
+        self.move(sum(dropped_sides))
+
+    def move(self, steps_amount):
+        self.position += steps_amount
+        if self.position > 40:
+            self.money += 2000
+            self.position -= 40
+
+    def pay_rent(self, field: Field):
+        if self.money >= field.rent:
+            self.money -= field.rent
+
+    def __str__(self):
+        return self.name
+
+
+class Cube:
+    def __init__(self):
+        self.sides = [1, 2, 3, 4, 5, 6]
+
+    def roll(self):
+        return random.choice(self.sides)
+
+
+class Game:
+    def __init__(self):
+        # Players
+        self.p1 = Player(1, 'P1', 15000)
+        self.p2 = Player(2, 'P2', 15000)
+        # Board
+        self.game_board = Board()
+        # Groups
+        self.food_group = Group('food', self.game_board)
+        self.auto_group = Group('auto', self.game_board)
+        # hotel_group = Group('hotel')
+        # Fields
+        self.BK_field = Field(1, 'BK', 2400, self.food_group, 'classic', 220)
+        self.McD_field = Field(2, 'McD', 2500, self.food_group, 'classic', 220)
+        self.KFC_field = Field(3, 'KFC', 2600, self.food_group, 'classic', 250)
+
+        self.BMW_field = Field(4, 'BMW', 2000, self.auto_group, 'classic', 250)
+        self.Audi_field = Field(5, 'Audi', 2000, self.auto_group, 'classic', 250)
+        self.Ford_field = Field(6, 'Ford', 2000, self.auto_group, 'classic', 250)
+        self.Mercedes_field = Field(7, 'Mercedes', 2000, self.auto_group, 'classic', 250)
+        # Cubes
+        self.Cub1 = Cube()
+        self.Cub2 = Cube()
